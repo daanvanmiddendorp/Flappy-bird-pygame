80c3d0da51474ae577ffffb38bd512dfa48b7bfa
Your Name <you@example.com>
2023-11-17 11:59:40 +0100

    BG / Animatie toegevoegd

--- code/main.py
+++ code/main.py
@@ -1,4 +1,50 @@
 # Hier komt de code voor de main
 
-import pygame
+import pygame, sys, time
 from settings import *
+from sprites import BG
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+        pygame.display.set_caption("Flappy Bird")
+        self.clock = pygame.time.Clock()
+
+        #sprite groups
+        self.all_sprites = pygame.sprite.Group()
+        self.collision_sprites = pygame.sprite.Group()
+
+        #scale_factor
+        bg_height = pygame.image.load("graphics/environment/background.png").convert()
+        self.scale_factor = WINDOW_HEIGHT / bg_height.get_height()
+
+        # sprites setup
+        BG(self.all_sprites,self.scale_factor)
+    
+    def run(self):
+            last_time = time.time()
+            while True:
+                
+                    # delta time
+                    dt = time.time() - last_time
+                    last_time = time.time()
+
+                    # event loop
+                    for event in pygame.event.get():
+                        if event.type == pygame.QUIT:
+                            pygame.quit()
+                            sys.exit()
+                    
+                    # game logic
+                    self.display_surface.fill((0,0,0))
+                    self.all_sprites.update(dt)
+                    self.all_sprites.draw(self.display_surface)
+
+                    pygame.display.update()
+                    self.clock.tick(FRAME_RATE)
+
+
+if __name__ == "__main__":
+    game = Game()
+    game.run()
--- code/sprites.py
+++ code/sprites.py
@@ -1,1 +1,25 @@
 # hier komt de code voor de sprites
+import pygame
+from settings import *
+
+class BG(pygame.sprite.Sprite):
+    def __init__(self, groups, scale_factor):
+        super().__init__(groups)
+        bg_image = pygame.image.load("graphics/environment/background.png").convert()
+
+        full_height = bg_image.get_height() * scale_factor
+        full_width = bg_image.get_width() * scale_factor
+        full_sized_image = pygame.transform.scale(bg_image,(int(full_width),int(full_height))) # scale de image naar de juiste grootte
+      
+        self.image = pygame.Surface((full_width * 2,full_height)) 
+        self.image.blit(full_sized_image,(0,0))
+        self.image.blit(full_sized_image,(full_width,0))
+      
+        self.rect = self.image.get_rect(topleft=(0,0)) # topleft is een x en y waarde in 1 variabele
+        self.pos = pygame.math.Vector2(self.rect.topleft) # vector2 is een x en y waarde in 1 variabele
+
+    def update(self, dt):
+        self.pos.x -= 300 * dt
+        if self.rect.centerx <= 0:
+            self.pos.x = 0
+        self.rect.x = round(self.pos.x)
